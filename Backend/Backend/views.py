6753fa0952be41afcc39b2395a0ce29f12bac1ee
Sridip Tah <164662749+Sridip-99@users.noreply.github.com>
2025-09-29 04:03:24 +0530

    Update views.py

--- Backend/Backend/views.py
+++ Backend/Backend/views.py
@@ -1,14 +1,14 @@
-from django.core.mail import send_mail
-from django.conf import settings
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 import json
+from .utils.email_sender import send_email
+from django.conf import settings
 
 @csrf_exempt
 def contact_view(request):
     if request.method == "POST":
         try:
-            data = json.loads(request.body.decode("utf-8"))  # decode for safety
+            data = json.loads(request.body.decode("utf-8"))
         except Exception as e:
             return JsonResponse({"status": "error", "message": f"Invalid JSON: {str(e)}"}, status=400)
 
@@ -20,17 +20,11 @@ def contact_view(request):
             return JsonResponse({"status": "error", "message": "All fields are required"}, status=400)
 
         subject = f"{name} sent you a message from your Portfolio"
+        html_content = f"<p><b>Name:</b> {name}</p><p><b>Email:</b> {email}</p><p><b>Message:</b><br>{message}</p>"
 
-        try:
-            send_mail(
-                subject,
-                message=f"Email: {email}\n\nMessage:\n{message}\n\nName:\n{name}",
-                from_email=settings.EMAIL_HOST_USER,
-                recipient_list=[settings.EMAIL_RECEIVER],
-                fail_silently=False,
-            )
-        except Exception as e:
-            return JsonResponse({"status": "error", "message": f"Email sending failed: {str(e)}"}, status=500)
+        result = send_email(settings.EMAIL_RECEIVER, subject, html_content)
+        if not result:
+            return JsonResponse({"status": "error", "message": "Email sending failed"}, status=500)
 
         return JsonResponse({"status": "success", "message": "Email sent successfully!"}, status=200)
 
