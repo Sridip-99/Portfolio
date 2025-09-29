7efcc8dedae883c22d96cf653003bc2f95ec8adf
Sridip Tah <164662749+Sridip-99@users.noreply.github.com>
2025-09-29 10:36:20 +0530

    Update Certifications.jsx

--- Frontend/src/sections/Certifications.jsx
+++ Frontend/src/sections/Certifications.jsx
@@ -64,7 +64,7 @@ const Certifications = () => {
                         animateOpacity
                         scale={.8}
                         threshold={0.1}
-                        delay={0.3 + (idx % 2 === 1 ? 0.2 : 0)}
+                        delay={0 + (idx >= 2 ? (idx - 2) * 0.3 : idx * 0.3)}
                     > 
                         <CertificationCard
                             id={elem.id}
@@ -123,4 +123,5 @@ const Certifications = () => {
 
 
 export default Certifications
+
 
