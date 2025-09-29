0451b061949e73c6d95bcfe47f3b62893ac6924a
Sridip Tah <164662749+Sridip-99@users.noreply.github.com>
2025-09-29 13:41:47 +0530

    Update urls.py

--- Backend/Backend/urls.py
+++ Backend/Backend/urls.py
@@ -16,12 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from . import views
 from Backend.views import contact_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("ping/", views.ping, name="ping"),
     path('contact/', contact_view, name="contact"),
 ]
+
 
 
 
