4c8e7a421fddca044cc9dbef8b3a832102579959
Sridip Tah <164662749+Sridip-99@users.noreply.github.com>
2025-09-29 13:38:45 +0530

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,22 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 import Header from './sections/Header'
 import Main from './sections/Main'
 import Footer from './sections/Footer'
 
+const API_URL = import.meta.env.VITE_API_URL;
+const baseURL = API_URL.replace(/\/+$/, ""); // Remove trailing slashes if any
+
 function App() {
+  
+  useEffect(() => {
+    // jaise hi portfolio load hoga, ye call backend ko wake karega
+    fetch(`${baseURL}/ping`, {
+      method: "GET",
+    })
+      .then(() => console.log("Backend wake-up request sent ✅"))
+      .catch((err) => console.error("Wake-up failed ❌", err));
+  }, []);
   
   return (
     <>
