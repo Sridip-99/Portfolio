3c68a850ccda06d183e9d329c0cc8551cabf414d
Sridip Tah <164662749+Sridip-99@users.noreply.github.com>
2025-09-29 10:32:16 +0530

    Update Education.jsx

--- Frontend/src/sections/Education.jsx
+++ Frontend/src/sections/Education.jsx
@@ -61,7 +61,7 @@ const Education = () => {
                         reverse={true}
                         duration={1}
                         ease="power3.out"
-                        initialOpacity={0.2}
+                        initialOpacity={0.5}
                         animateOpacity
                         scale={.8}
                         threshold={0.1}
@@ -87,4 +87,5 @@ const Education = () => {
 
 
 export default Education
+
 
