db4a3ff85f787d5388e72b8085ef30ccbbbdfee5
Sridip Tah <164662749+Sridip-99@users.noreply.github.com>
2025-10-01 21:40:12 +0530

    Add files via upload

---
+++ Frontend/src/components/Preloader.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Preloader = () => {
+  return (
+    <>
+        <div id="preloader">
+          <img src="./logo.gif" alt="Loading…" />
+        </div>
+    </>
+  )
+}
+export default Preloader
