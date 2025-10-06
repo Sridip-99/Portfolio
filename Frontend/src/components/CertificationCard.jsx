eb2a4eb3a9c9c189ef7fb5da89296c8cec702cf6
Sridip Tah <164662749+Sridip-99@users.noreply.github.com>
2025-10-06 11:25:44 +0530

    Update CertificationCard.jsx

--- Frontend/src/components/CertificationCard.jsx
+++ Frontend/src/components/CertificationCard.jsx
@@ -5,7 +5,7 @@ const CertificationCard = (props) => {
     <>
         <div className='card3'>
             <div className='w-full border-2 border-slate-600 rounded-md overflow-hidden shadow-lg hover:shadow-2xl transition-shadow duration-300 bg-gradient-to-b from-slate-900 via-slate-800 to-slate-900'>
-                <img src={props.image} alt={props.imgAltText} className='brightness-95 w-full h-full overflow-hidden object-cover'/>
+                <img loading="lazy" src={props.image} alt={props.imgAltText} className='brightness-95 w-full h-full overflow-hidden object-cover'/>
                 <div className='pt-1 pb-2 px-2 rounded-sm bg-slate-950 '>
                     <h1 className='w-full text-center text-slate-400 mt-1 text-xl md:text-2xl font-semibold'>{props.name}</h1>
                     <p className='w-full text-center font-light text-zinc-400'>Issued by: <span className='text-slate-300'>{props.issued}</span></p>
@@ -18,3 +18,4 @@ const CertificationCard = (props) => {
 
 
 export default CertificationCard
+
