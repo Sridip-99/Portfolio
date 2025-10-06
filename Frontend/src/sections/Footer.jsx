c95005ba8d8ba2eef9ea4dedd76be411f52ce149
Sridip Tah <164662749+Sridip-99@users.noreply.github.com>
2025-10-06 20:23:52 +0530

    Update Footer.jsx

--- Frontend/src/sections/Footer.jsx
+++ Frontend/src/sections/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <>
-      <div className='w-full mx-auto my-0 p-4 text-slate-300 bg-slate-900 text-md md:text-xl text-center z-50 rounded-tl-2xl rounded-tr-2xl'>
+      <div className='w-full mx-auto my-0 p-4 text-slate-300 bg-slate-900 text-md md:text-xl text-center rounded-tl-2xl rounded-tr-2xl' style={{ z-index: "1000" }}>
         <p>
           <a href="https://sridiptah99.netlify.app" className='hover:text-slate-400'>Sridip Tah</a>
           <span className='font-semibold'> || </span>
@@ -17,5 +17,6 @@ const Footer = () => {
 
 
 export default Footer
+
 
 
